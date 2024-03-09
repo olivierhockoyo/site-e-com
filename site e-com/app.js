b636d5043a79f38653b6d02d3448e519d4baabe4
olivierhockoyo <150544817+olivierhockoyo@users.noreply.github.com>
2024-03-09 18:05:10 +0100

    Add files via upload
    site pour ventre des vetements

---
+++ site e-com/app.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('#manu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
